a793653b878c22d32b905924c91955510f1d78a9
Damion Cole <36947656+crazyjayj17@users.noreply.github.com>
2018-07-03 11:06:46 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
-const phraseUl = document.querySelector('#phrase ul')
+
+const phrase = document.getElementById('phrase')
+
 let missed = 0;
 const overlay = document.getElementById('overlay');
 const reset = document.getElementsByClassName('btn__reset')[0];
+const keyRow = document.getElementsByClassName('keyrow');
+const buttons = document.querySelectorAll('button');
 let phrases = ['A fish out of water',
                'Different kettle of fish',
                'Let sleeping dogs lie',
@@ -35,22 +38,47 @@ const phraseArray = getRandomPhraseAsArray(phrases);
 function addPhraseToDisplay(arr) {
   for(let i = 0; i < arr.length; i += 1) {
     let li = document.createElement('li');
+    const phraseUl = document.querySelector('#phrase ul');
     li.textContent = arr[i];
     phraseUl.appendChild(li);
-    if(arr[i] == ' ') {
+
+    if( arr[i].includes(' ') ) {
      li.className = 'space';
     } else {
      li.className = 'letter';
     }
   }
+
 }
 addPhraseToDisplay(phraseArray);
 
-function checkLetter() {
-
+function checkLetter(but) {
+  for(let i = 0; i < but.length; i += 1) {
+    if(but.className == 'letter') {
+      but[i].className = 'show';
+    } else {
+      return null;
+    }
+  }
 }
 
+// button.addEventListener('click', () => {
+//   //if(event.target.tagName == 'BUTTON') {
+//     for(let i = 0; i < button.length; i += 1) {
+//      button[i].style.background = 'green';
+//      // but.className = 'chosen';
+//     }
+//   //}
+// });
 
+buttons.addEventListener('click', (event) => {
+
+    for(let i = 0; i < buttons.length; i += 1) {
+      buttons[i].style.background = 'green';
+      buttons[i].className = 'chosen';
+    }
+
+});
 
 
 
